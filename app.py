e50130c0901185f696a869580dfaa3eacad4be93
dessanhemrayev <43703214+dessanhemrayev@users.noreply.github.com>
2022-09-06 14:29:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 path = ""
 content = os.listdir(path)
+content_upload = []
 for module in content:
     # os.system(f'cd foor_git/{module} && git init')
-    os.system(f'COPY ".gitignore" foor_git\{module}')
+    # os.system(f'COPY ".gitignore" foor_git\{module}')
+    # os.system(f'MOVE foor_git\{module} upload')
+    os.system(f'git clone {module}')
